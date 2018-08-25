0dbf76e0987cd76b8fba1a0ed52aae14004875de
Tokuhisa1 <TokuhisaWinston@Outlook.com>
2018-08-25 14:44:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const driver = 'bob';
+// Strings can be logged in the [browser] console
+console.log("this code was called");
+// Variables can be logged in the console
+console.log(driver);
+// Console.log can take multiple arguments
+console.log(driver, 'is the driver variable name');
